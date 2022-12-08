f3c951c14c9b470371ccca124e66435f6099744b
Leonardo Conroy <leeleonard936@gmail.com>
2022-12-08 11:01:14 -0500

    image is in viewrecipe, errors are gone

--- app/src/main/java/towson/cosc435/cookbook/screens/Cookbook.kt
+++ app/src/main/java/towson/cosc435/cookbook/screens/Cookbook.kt
@@ -24,7 +24,7 @@ import towson.cosc435.cookbook.database.Recipe
 import towson.cosc435.cookbook.navigation.NavRoutes
 import towson.cosc435.cookbook.ui.theme.Purple100
 
-val API_URL = "https://source.unsplash.com/random/?"
+
 @Composable
 fun Cookbook(
     allRecipes: List<Recipe>,
@@ -55,6 +55,7 @@ fun RecipeRow(
 ) {
     val jsonString = Gson().toJson(recipe)
     Card(modifier = Modifier.padding(10.dp).clickable { navController.navigate(NavRoutes.ViewRecipe.route + "/$jsonString" ) }, backgroundColor = Purple100) {
+        val API_URL = "https://source.unsplash.com/random/?"
         var url = API_URL + recipe.recipeName.toString()
         Column(modifier = Modifier.padding(10.dp)) {
             Row(
