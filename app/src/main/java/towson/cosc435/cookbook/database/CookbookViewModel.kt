7c2a47adfd5f3048a67f6c12a4026a9037bfd00a
Leonardo Conroy <leeleonard936@gmail.com>
2022-12-07 19:34:30 -0500

    network test works

--- app/src/main/java/towson/cosc435/cookbook/database/CookbookViewModel.kt
+++ app/src/main/java/towson/cosc435/cookbook/database/CookbookViewModel.kt
@@ -10,8 +10,11 @@ import android.util.Log
 import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import coil.compose.AsyncImagePainter
 import coil.compose.rememberAsyncImagePainter
+import kotlinx.coroutines.NonCancellable.isActive
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 //help from https://www.answertopia.com/jetpack-compose/a-jetpack-compose-room-database-and-repository-tutorial/
 
@@ -30,20 +33,27 @@ class CookbookViewModel(application: Application) : ViewModel() {
         val netreq = NetworkRequest.Builder()
             .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
             .build()
+        viewModelScope.launch {
+            while (isActive) {
         cm.requestNetwork(netreq, object: ConnectivityManager.NetworkCallback() {
-            override fun onAvailable(network: Network) {
-                super.onAvailable(network)
-                val nc = cm.getNetworkCapabilities(network)
-                if (nc == null) {
-                    connectionStatus = false
-                }
-            }
+
+                    override fun onAvailable(network: Network) {
+                        super.onAvailable(network)
+                        val nc = cm.getNetworkCapabilities(network)
+                        if (nc == null) {
+                            connectionStatus = false
+                        }
+                    }
+
             override fun onLost(network: Network) {
                 super.onLost(network)
                 Log.d(ViewModel::class.java.simpleName, "Network unavailable")
                 connectionStatus = false
             }
         })
+                delay(60 * 1000L)
+            }
+        }
     }
 
     fun insertRecipe(recipe: Recipe) {
